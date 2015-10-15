8edfbc952a4eb9b486cd4c8303d870edb5bf2af8
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-10-15 17:52:27 -0500

    Add happiness exercises examples

---
+++ spec/features/happiness_exercises_spec.rb
@@ -0,0 +1,71 @@
+# filename: happiness_exercises_spec.rb
+
+describe 'Participant opens app', type: :feature do
+  after do
+    page.execute_script('localStorage.clear()')
+  end
+
+  it "completes 'Three Good Things'" do
+    visit 'localhost:8000'
+    insert_all(CessationDate::DATE_1, Sessions::SESSION_1)
+    insert(Exercises::KEY, Exercises::GOOD_THINGS)
+    page.execute_script('window.location.reload()')
+    find('.wide.btn.btn-default', text: 'THREE GOOD THINGS').click
+    fill_in 'answer_0', with: 'First good thing'
+    fill_in 'answer_1', with: 'Second good thing'
+    fill_in 'answer_2', with: 'Third good thing'
+    find('#save_button').click
+    expect(page).to have_css('#success-alert')
+  end
+
+  it "completes 'Experiencing Kindness'" do
+    visit 'localhost:8000'
+    insert_all(CessationDate::DATE_1, Sessions::SESSION_1)
+    insert(Exercises::KEY, Exercises::EXP_KIND)
+    page.execute_script('window.location.reload()')
+    find('.wide.btn.btn-default', text: 'EXPERIENCING KINDNESS').click
+    fill_in 'answer_0', with: 'First kindness experience'
+    fill_in 'answer_1', with: 'Second kindness experience'
+    find('#save_button').click
+    expect(page).to have_css('#success-alert')
+  end
+
+  it "completes 'Savoring'" do
+    visit 'localhost:8000'
+    insert_all(CessationDate::DATE_1, Sessions::SESSION_1)
+    insert(Exercises::KEY, Exercises::SAVORING)
+    page.execute_script('window.location.reload()')
+    find('.wide.btn.btn-default', text: 'SAVORING').click
+    fill_in 'answer_0', with: 'First savoring'
+    fill_in 'answer_1', with: 'Second savoring'
+    find('#save_button').click
+    expect(page).to have_css('#success-alert')
+  end
+
+  it 'uses help menus' do
+    visit 'localhost:8000'
+    insert_all(CessationDate::DATE_1, Sessions::SESSION_1)
+    insert(Exercises::KEY, Exercises::GOOD_THINGS)
+    page.execute_script('window.location.reload()')
+    find('.wide.btn.btn-default', text: 'THREE GOOD THINGS').click
+    find('.text-transform-default', text: 'Examples').click
+    expect(page).to have_css('h3', text: 'Examples')
+    find('.btn.btn-primary', text: 'CLOSE').click
+    find('.text-transform-default', text: 'How does happiness help me quit smoking?').click
+    expect(page).to have_css('h3', text: 'Why Does Happiness Help?')
+    find('.btn.btn-primary', text: 'CLOSE').click
+    find('.text-transform-default', text: 'How does this exercise help me quit smoking?').click
+    expect(page).to have_css('h3', text: 'Why Do 3 Good Things?')
+    find('.btn.btn-primary', text: 'CLOSE').click
+  end
+
+  it 'exits happiness exercise without saving' do
+    visit 'localhost:8000'
+    insert_all(CessationDate::DATE_1, Sessions::SESSION_1)
+    insert(Exercises::KEY, Exercises::GOOD_THINGS)
+    page.execute_script('window.location.reload()')
+    find('.wide.btn.btn-default', text: 'THREE GOOD THINGS').click
+    find('#exit_button', text: 'EXIT').click
+    expect(page).to have_content '4 days until quit day'
+  end
+end
